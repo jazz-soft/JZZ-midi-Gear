189d23ee593a6c22c3896e5816a24c9add6844d1
Sema <sema@pyva.net>
2018-03-21 23:09:38 -0400

    Added JShint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: { sub: true },
+      all: ['javascript/*.js']
+    },
     assemble: {
       vendors: 'data/vendors.txt',
       models: 'data/models.txt',
@@ -16,6 +20,7 @@ module.exports = function(grunt) {
     }
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('assemble', require('./data/assemble.js')(grunt));
   grunt.registerTask('default', ['assemble', 'uglify']);
 };
